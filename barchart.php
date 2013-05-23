24ebd65d2fb8c7dd337f6c0a596cf6b54dc7a871
Denis Omeri TOK PC <denisomeri@gmail.com>
2013-05-23 11:04:50 +0200

    Division by zero error ...
    Catching error if input array has no elements

--- barchart.php
+++ barchart.php
@@ -4,7 +4,7 @@
 // Barchart Chart Generator LIBRARY
 // by Denis Omeri (http://denis.erlotto.com/)
 //
-// version: 1.0 
+// version: 1.1 
 //---------------------------------------------------------------
 
 /**
@@ -47,35 +47,42 @@ class barchart {
 	public function generate_chart($data, $width, $height){
 		
 		$this->data 	= $data;
-		$this->width	= $width;
-		$this->height	= $height;
+		$html			= "";
 		
-		$one_bar_full_w	= $this->width / count($this->data);
-		$margin_w		= 0.2 * $one_bar_full_w;
-		$bar_w			= 0.6 * $one_bar_full_w;
+		if (count($this->data)>0):
 		
-		$margin_h		= 0 * $this->height;
-		$bar_h			= 1 * $this->height;
-		
-		$bars = '';
-		
-		foreach($this->data as $key => $value){
-		
-			$thisbh = ($value * $bar_h)/max($this->data);
-		
-			$bars = $bars.'<div style="width:'.$bar_w.'px; height:'.$thisbh.'px; float:left; margin:'.($margin_h+$bar_h-$thisbh).'px '.$margin_w.'px; margin-bottom: 0px; text-align:center; overflow: hidden; ">
+			$this->width	= $width;
+			$this->height	= $height;
 			
-			<span style="font-size:10px; height:10px; display:block; margin:3px 0; line-height: 10px;">'.$value.'</span>
-			<div style="-moz-box-shadow: 0 0 5px #888;
--webkit-box-shadow: 0 0 5px #888;
-box-shadow: 0 0 5px #888; border: 1px solid #f1eaea; background-color: #0066ff; width:100%; height:'.($thisbh-42).';"></div>
-			<span style="font-size:10px; height:10px; display:block; margin:3px 0; line-height: 10px;">'.$key.'</span>
+			$one_bar_full_w	= $this->width / count($this->data);
+			$margin_w		= 0.2 * $one_bar_full_w;
+			$bar_w			= 0.6 * $one_bar_full_w;
 			
-			</div>';
-		}
+			$margin_h		= 0 * $this->height;
+			$bar_h			= 1 * $this->height;
+			
+			$bars = '';
+			
+			foreach($this->data as $key => $value){
+			
+				$thisbh = ($value * $bar_h)/max($this->data);
+			
+				$bars = $bars.'<div style="width:'.$bar_w.'px; height:'.$thisbh.'px; float:left; margin:'.($margin_h+$bar_h-$thisbh).'px '.$margin_w.'px; margin-bottom: 0px; text-align:center; overflow: hidden; ">
+				
+				<span style="font-size:10px; height:10px; display:block; margin:3px 0; line-height: 10px;">'.$value.'</span>
+				<div style="-moz-box-shadow: 0 0 5px #888;
+	-webkit-box-shadow: 0 0 5px #888;
+	box-shadow: 0 0 5px #888; border: 1px solid #f1eaea; background-color: #0066ff; width:100%; height:'.($thisbh-42).';"></div>
+				<span style="font-size:10px; height:10px; display:block; margin:3px 0; line-height: 10px;">'.$key.'</span>
+				
+				</div>';
+			}
+			
+			$html = '<div style="background-image:url('.$this->imagepath.'/chart_bg.png); border: 1px solid #ccc; width:'.$this->width.'px; height:'.$this->height.'px; ">'.$bars.'</div>';
+			//
 		
-		$html = '<div style="background-image:url('.$this->imagepath.'/chart_bg.png); border: 1px solid #ccc; width:'.$this->width.'px; height:'.$this->height.'px; ">'.$bars.'</div>';
-		//
+		endif;
+		
 		return $html;
 	}
 	
